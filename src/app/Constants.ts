ac139a98f7bc9d1b6e711334600425cb2a7ed5f4
koalter <heavens_drive090@hotmail.com>
2020-07-18 05:00:00 -0300

    added pdf export

---
+++ src/app/Constants.ts
@@ -0,0 +1,11 @@
+export const CONSTANTS: string[] = [
+    'Resumen de cuenta', 
+    'cantidad', 
+    'precio', 
+    'SUBTOTAL', 
+    '% descuentos', 
+    'TOTAL', 
+    'Tu entrega es en: ', 
+    'Caballito', 
+    'Paternal/Villa Crespo'
+];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 import { Articulo } from './Articulo';
 import { mock_articulos } from './mock';
+import { CONSTANTS } from './Constants';
 
 @Component({
   selector: 'app-root',
@@ -20,25 +22,31 @@ export class AppComponent {
   totalDescuento: number = 0;
   listaDescuentos: number[] = [];
   precioFinal: number;
-  entrega = 0;
+  entrega = NaN;
+  
+  get CONSTANTS() { return CONSTANTS }
 
   ngOnInit() {
     this.sumar();
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
+    console.log(pdfMake);
   }
 
   add() {
-    let nuevoArticulo: Articulo = {
-      id: this.articulos.length - 1,
-      nombre: this.nombre ?? "Varios",
-      cantidad: parseInt(this.cantidad),
-      precio: parseFloat(this.precio)
-    };
-    console.log(nuevoArticulo);
-    this.articulos.push(nuevoArticulo);
-    this.nombre = '';
-    this.cantidad = '';
-    this.precio = '';
-    this.sumar();
+    if (this.cantidad && this.precio) {
+      let nuevoArticulo: Articulo = {
+        id: this.articulos.length - 1,
+        nombre: this.nombre ?? "Varios",
+        cantidad: parseInt(this.cantidad),
+        precio: parseFloat(this.precio)
+      };
+      console.log(nuevoArticulo);
+      this.articulos.push(nuevoArticulo);
+      this.nombre = '';
+      this.cantidad = '';
+      this.precio = '';
+      this.sumar();
+    }
   }
 
   private sumar() {
@@ -71,5 +79,49 @@ export class AppComponent {
 
   public exportar() {
     console.log(this.entrega);
+    const documentDefinition = { 
+      content: [
+        { text: CONSTANTS[0], fontSize: 18, bold: true },
+        {
+          table: {
+            headerRows: 1,
+            body: this.buildTableBody()
+          },
+          layout: 'noBorders'
+        }
+      ]
+    };
+    pdfMake.createPdf(documentDefinition).open();
+    // pdfMake.createPdf(documentDefinition).download('helloworld.pdf');
+  }
+
+  private buildTableBody(): object[] {
+    let table: object[] = [];
+    let thead = [{ text: '' }, { text: CONSTANTS[1], alignment: 'right', bold: true }, { text: CONSTANTS[2], alignment: 'right', bold: true }];
+    let subtotal: object[] = [];
+    
+    table.push(thead);
+
+    this.articulos.forEach(art => {
+      table.push([{ text: art.nombre }, { text: art.cantidad+'u.', alignment: 'right' }, { text: '$ '+art.precio, alignment: 'right' }]);
+    });
+
+    table.push([{ text: '', colSpan: 3, margin: [0, 0, 0, 16] }]);
+
+    if (this.totalCantidad > 0 && this.subtotal > 0) {
+      subtotal = [{ text: CONSTANTS[3], alignment: 'right' }, { text: this.totalCantidad.toString()+'u.', alignment: 'right' }, { text: '$ '+this.subtotal.toString(), alignment: 'right' }];
+      table.push(subtotal);
+    }
+
+    table.push([{ text: CONSTANTS[4], alignment: 'right' }, {}, {}]);
+    
+    this.listaDescuentos.forEach(des => {
+      table.push([{ text: des.toString(), colSpan: 3, alignment: 'right' }, {}, {}]);
+    });
+
+    table.push([{ text: CONSTANTS[5], alignment: 'right', bold: true }, { text: '$ '+this.precioFinal.toString(), colSpan: 2, alignment: 'right', bold: true }]);
+
+    table.push([{ text: CONSTANTS[6], margin: [0, 24, 0, 0], alignment: 'right' }, { text: CONSTANTS[this.entrega], alignment: 'right', colSpan: 2, margin: [24, 24, 0, 0] }]);
+    return table;
   }
 }
