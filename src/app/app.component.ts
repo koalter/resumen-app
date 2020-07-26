3e3ee861361f19a7004918c12cd6d81b57dd8fa6
koalter <heavens_drive090@hotmail.com>
2020-07-26 04:59:45 -0300

    added condition for download()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -135,7 +135,9 @@ export class AppComponent {
     
     const pdf = pdfMake.createPdf(documentDefinition);
     pdf.open();
-    pdf.download();
+    if (environment.production) {
+      pdf.download();
+    }
   }
 
   private buildTableBody(): object[] {
@@ -199,31 +201,6 @@ export class AppComponent {
     }
 
     return table;
-  }
-
-  private getBase64ImageFromURL(url) {
-    return new Promise((resolve, reject) => {
-      var img = new Image();
-      img.setAttribute("crossOrigin", "anonymous");
-      img.onload = () => {
-        var canvas = document.createElement("canvas");
-        canvas.width = img.width;
-        canvas.height = img.height;
-
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0);
-
-        var dataURL = canvas.toDataURL("image/png");
-
-        resolve(dataURL);
-      };
-
-      img.onerror = error => {
-        reject(error);
-      };
-
-      img.src = url;
-    });
   }
 
   private generarFondo() {
