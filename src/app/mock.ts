8b4f17489e2fc3ab438c5439500f098a4d3c9b87
koalter <heavens_drive090@hotmail.com>
2020-07-26 04:22:22 -0300

    fixed width articulo, added otro, added background

--- src/app/mock.ts
+++ src/app/mock.ts
@@ -2,31 +2,30 @@ export let mockRepository = {
     articulos: [
         {
             id: 1,
-            nombre: "Gorro termico con piel y tachas",
-            cantidad: 1,
-            precio: 1
+            nombre: "Un articulo con descripcion moderada",
+            cantidad: 100,
+            precio: 12345
         },
         {
             id: 2,
-            nombre: "Gorro termico con lentejuela",
-            cantidad: 1,
-            precio: 1
+            nombre: "Un articulo con una descripcion extremadamente larga, especifica, detallada y supercalifragilisticoespialidosa",
+            cantidad: 10,
+            precio: 10.00
         },
         {
             id: 3,
-            nombre: "Gorro simple rayado",
+            nombre: "Un articulo simple",
             cantidad: 1,
-            precio: 1
+            precio: 198.99
         }
     ],
     descuentos: [
         {
             key: 1,
-            value: 1
+            value: 30.01
         }
     ],
     zonasDeEntrega: [
-        'Caballito',
         'Cid Campeador',
         'Torino'
     ]
